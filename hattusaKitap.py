8bde1eca75c79fa2a7eaa1040435b8e79d48e40a
o d <ninjakaplumbag@gmail.com>
2018-12-21 00:24:35 +0300

    r eklendi

--- hattusaKitap.py
+++ hattusaKitap.py
@@ -4,7 +4,7 @@ https://hattusa.org/ sitesinden tüm kitap listesi
 import requests
 from bs4 import BeautifulSoup
 
-# Sitede toplamda 68 sayfalık kitap var
+# 68 sayfalık kitap var
 for i in range(1, 69):
     headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0;"
                "Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0"}
@@ -14,7 +14,7 @@ for i in range(1, 69):
     soup = BeautifulSoup(data, features="html.parser")
     # kitaplar sitede list-group-item class'ında duruyor
     kitaplar = soup.find_all(class_="list-group-item")
-    out = "C:\\Users\\onurd\\Desktop\\projects\\python\\scrap\\kitap.txt"
+    out = r"C:\Users\onurd\Desktop\projects\python\scrap\kitap.txt"
     # Her kitapta bulunan fazladan boşlukları ve \n karakteri düzeltelim
     for j in range(len(kitaplar)):
         kitap = kitaplar[j].contents[1].contents[0]
